9732067688c1b4cd4b33f04f6642b405bd9d8eb6
tshepo <lovekilledtshepo@gmail.com>
2023-06-12 09:22:08 +0200

    adjustments on home and main

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,7 @@
-import 'package:flutter/material.dart'
-    show
-        AppBar,
-        BuildContext,
-        Center,
-        Column,
-        MainAxisAlignment,
-        Scaffold,
-        State,
-        StatefulWidget,
-        Text,
-        Theme,
-        Widget;
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -42,24 +24,20 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(widget.title),
-      ),
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: MapScreen(
         )
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     Text("hello"),
-        //   ],
-        // ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,34 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart'
-    show AnimatedSplashScreen, SplashTransition;
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:test_app_tshepo/home.dart';
 import 'package:test_app_tshepo/splash.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// DotEnv dotenv = DotEnv() is automatically called during import.
-// If you want to load multiple dotenv files or name your dotenv object differently, you can do the following and import the singleton into the relavant files:
-// DotEnv another_dotenv = DotEnv()
 
 Future main() async {
-  // To load the .env file contents into dotenv.
-  // NOTE: fileName defaults to .env and can be omitted in this case.
-  // Ensure that the filename corresponds to the path in step 1 and 2.
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
-  //...runapp
 }
 
-// void main() {
-
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
-        useMaterial3: true,
-      ),
-      home: AnimatedSplashScreen(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 0, 0, 0)),
+          useMaterial3: true,
+        ),
+        home: AnimatedSplashScreen(
           splash: SvgPicture.asset("assets/codingTest/quickloc8.svg"),
           duration: 3500,
           splashTransition: SplashTransition.scaleTransition,
-          backgroundColor: Colors.white,
-          nextScreen: const MyHomePage(title: "COMPTON"),
-    ));
+          backgroundColor: Colors.black,
+          nextScreen: const MyHomePage(title: "ends"),
+        ));
   }
 }
-
-// class MapScreen extends StatefulWidget {
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _MapScreenState creat eState() => _MapScreenState();
-// }
-
-// class _MapScreenState {}
