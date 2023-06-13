3022376ea8a34ba9a30b3ec93b4074671b8b3e75
tshepo <lovekilledtshepo@gmail.com>
2023-06-13 17:34:44 +0200

    messages

--- lib/home.dart
+++ lib/home.dart
@@ -2,36 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
+
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
   static const _initialCameraPosition = CameraPosition(
-    target: LatLng(37.773972, -122.4312937),
+    target: LatLng(37.773972, -99.4312937),
     zoom: 11.5,
   );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: GoogleMap(
-          myLocationButtonEnabled: false,
-          zoomControlsEnabled: false,
-          initialCameraPosition: _initialCameraPosition),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            myLocationButtonEnabled: true,
+            zoomControlsEnabled: true,
+            initialCameraPosition: _initialCameraPosition,
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Add your button's onPressed logic here
+              },
+              child: Icon(Icons.message),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: MapScreen(
-        )
+        child: MapScreen(),
       ),
     );
   }
